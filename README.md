5e7fc237bbabe791cfb8236fed7b15825b8dfba7
fanna <fanna@fannadevelopment@gmail.com>
2024-10-17 14:15:28 +0200

    Reset hard and add all files

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# GitHub-Workshop
+# Git(Hub)-Workshop
+
+This repository serves as the start point for the Git(Hub) Workshops. There are three workshops, basics, intermediate, advanced that gives you *some* knowledge. It is correct that you read 'some knowledge' since git is old enough to drink alcohol; meaning that you can think of, probably is in git(hub).
+
+## Basics
+The interactive presentation can be found [here](https://www.menti.com/al7uavhktfah)
+
+It contains the following topics:
+- SSH keys, git configuration
+- Fork, clone
+- Branches, origin, remote, HEAD, base
+- Push, pull, fetch, upstream, merge
+- Resolving merge conflicts
+- Pull requests
+- Final Assignment
+
+## Intermediate
+The interactive presentation can be found [here](https://www.menti.com/alfx9nuhimbm) 
+
+It contains the following topics:
+- Fork
+- Logging
+- Squash, rebase, cherry-picking
+- Revert and reset
+- Merge strategies
+
+## Advanced
+The interactive presentation can be found [here](https://www.menti.com/al68a8nyq91p)
+
+It contains the following topics:
+- Subtrees
+- LFS
+- Prune
+- Submodules
+- Archiving
---
+++ clutter.sh
@@ -0,0 +1,44 @@
+git checkout -b 'new-feature'
+printf "1. Discovery of Git" >> story.txt
+git add . && git commit -m "start"
+printf "\n" >> story.txt
+git add . && git commit -m "spaces"
+printf "Once upon a time, in a digital realm, a young coder named Jeroen stumbled upon an ancient artifact called Git, said to hold the power of collaboration over code." >> story.txt
+git add . && git commit -m "story"
+printf "\n\n" >> story.txt
+git add . && git commit -m "new section"
+printf "2. The Message" >> story.txt
+git add . && git commit -m "the message"
+printf "\n" >> story.txt
+git add . && git commit -m "spaces"
+printf "While exploring a repository, he found a mysterious file, README.md, containing the message: Every word counts. Each change matters." >> story.txt
+git add . && git commit -m "story part 2"
+printf "\n\n" >> story.txt
+git add . && git commit -m "new section"
+printf "3. Conflict Arises" >> story.txt
+git add . && git commit -m "conflict"
+printf "\n" >> story.txt
+git add . && git commit -m "spaces"
+printf "As Jeroen learned to create branches and push changes, chaos ensued. He faced rival coder Jamie, who exclaimed, You’ve overwritten my changes!" >> story.txt
+git add . && git commit -m "story part 3"
+printf "\n\n" >> story.txt
+git add . && git commit -m "new section"
+printf "4. Finding Common Ground" >> story.txt
+git add . && git commit -m "Common ground"
+printf "\n" >> story.txt
+git add . && git commit -m "spaces"
+printf "Realizing the importance of communication, Jeroen suggested, Let’s resolve this together. They collaborated on the changes, carefully considering each word.!" >> story.txt
+git add . && git commit -m "working with jamie"
+printf "\n\n" >> story.txt
+git add . && git commit -m "new section"
+printf "5. A New Understanding" >> story.txt
+git add . && git commit -m "teamwork"
+printf "\n" >> story.txt
+git add . && git commit -m "spaces"
+printf "Through teamwork, they merged their work beautifully. Jeroen learned that Git was about collaboration and respect for every change, embracing the idea that in the world of Git, every word counts." >> story.txt
+git add . && git commit -m "story part"
+printf "\n\n" >> story.txt
+git add . && git commit -m "new section"
+printf "The End\n" >> story.txt
+git add . && git commit -m "end"
+git push -u origin HEAD
